1d3d1415ee763535da490b8bdc8dc280eaa70c37
maudyaulia <rizkymaudy16@gmail.com>
2023-12-19 17:20:09 +0700

    First Commit

---
+++ prosespost.php
@@ -0,0 +1,12 @@
+<?php
+ echo "<h2>Hasil Formulir Biodata</h2>";
+echo "<p>Nama :".$_POST['nama']."</p><br>";
+echo "<p>Alamat :".$_POST['alamat']."</p><br>";
+echo "<p>Umur :".$_POST['umur']."</p><br>";
+echo "<p>Jenis Kelamis :".$_POST['jenis_kelamin']."</p><br>";
+echo "<p>Pekerjaan :".$_POST['pekerjaan']."</p><br>";
+echo "<p>Email :".$_POST['email']."</p><br>";
+echo "<p>Telepon :".$_POST['telepon']."</p><br>";
+echo "<p>Hobi :".$_POST['hobi']."</p><br>";
+echo "<p>Tanggal Lahir :".$_POST['tanggal_lahir']."</p></br>";
+?>
